9cacd8e81a7b146baf1ce7ac99a2c446521091a1
Md Asharaf <718romanempire@gmail.com>
2024-12-29 18:31:15 +0530

    feat: Implement user message acceptance check and conditionally render message form

--- src/app/(root)/u/[username]/page.tsx
+++ src/app/(root)/u/[username]/page.tsx
@@ -20,11 +20,17 @@ import { useParams } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { useEffect, useState } from "react";
+
 export default function SendMessage() {
+    const [isUserAcceptingMessages, setIsUserAcceptingMessages] =
+        useState(false);
     const { username } = useParams();
+
     const form = useForm<z.infer<typeof MessageSchema>>({
         resolver: zodResolver(MessageSchema),
     });
+
     const submit = async (data: z.infer<typeof MessageSchema>) => {
         try {
             await axios.post("/api/send-message", {
@@ -46,65 +52,93 @@ export default function SendMessage() {
             });
         }
     };
+
     const { mutate: handleSubmit, isPending } = useMutation({
         mutationKey: ["messages", username],
         mutationFn: submit,
     });
+
+    useEffect(() => {
+        const isAcceptingMessages = async () => {
+            try {
+                const res = await axios.get(`/api/accept-message/`);
+                setIsUserAcceptingMessages(res.data?.isAcceptingMessages);
+            } catch (error: any) {
+                console.error(
+                    "Error checking if user is accepting messages: ",
+                    error.message
+                );
+            }
+        };
+        isAcceptingMessages();
+    }, []);
+
     return (
         <div className="flex-grow">
             <div className="border-[1px] p-4 rounded-lg shadow-xl relative max-w-lg mx-auto mt-20 space-y-4">
                 <h1 className="text-2xl font-bold text-center">
                     Send Message Anonymously
                 </h1>
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit((data) =>
-                            handleSubmit(data)
-                        )}
-                        className=" space-y-4"
-                    >
-                        <FormField
-                            control={form.control}
-                            name="title"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Title:</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            placeholder="enter a title"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
+
+                {!isUserAcceptingMessages ? (
+                    <div className="text-center text-red-600">
+                        <p>This user is currently not accepting messages.</p>
+                    </div>
+                ) : (
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit((data) =>
+                                handleSubmit(data)
                             )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="content"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Message:</FormLabel>
-                                    <FormControl>
-                                        <Textarea
-                                            placeholder="type a message"
-                                            {...field}
-                                            className="min-h-[200px]"
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button variant="default" type="submit">
-                            {isPending ? (
-                                <Loader2 className="animate-spin h-6 w-6" />
-                            ) : (
-                                "Send"
-                            )}
-                        </Button>
-                    </form>
-                </Form>
+                            className="space-y-4"
+                        >
+                            <FormField
+                                control={form.control}
+                                name="title"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Title:</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Enter a title"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="content"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Message:</FormLabel>
+                                        <FormControl>
+                                            <Textarea
+                                                placeholder="Type a message"
+                                                {...field}
+                                                className="min-h-[200px]"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button
+                                variant="default"
+                                type="submit"
+                                disabled={isPending}
+                            >
+                                {isPending ? (
+                                    <Loader2 className="animate-spin h-6 w-6" />
+                                ) : (
+                                    "Send"
+                                )}
+                            </Button>
+                        </form>
+                    </Form>
+                )}
             </div>
         </div>
     );
